4d2f4d0e88666bc8d5bce0e4263859ed8e94dbc5
DimitryRukhadze <lokiinflames@gmail.com>
2022-06-11 18:28:37 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
     image = models.ImageField(null=True)
 
     def __str__(self):
         return self.title
+
 
 class PokemonEntity(models.Model):
     lat = models.FloatField(blank=True)
